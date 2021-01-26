9118340476ea4a7b04b681092849bfd8a327d94e
febys <napsterfly@gmail.com>
2021-01-26 23:12:28 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { DentalServicesComponent } from './dental-services/dental-services.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { ScrollToTheTopComponent } from './scroll-to-the-top/scroll-to-the-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    DentalServicesComponent,
+    AboutUsComponent,
+    DoctorComponent,
+    TestimonialComponent,
+    BlogComponent,
+    ContactUsComponent,
+    FooterComponent,
+    ScrollToTheTopComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dental-services/dental-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dental-services',
+  templateUrl: './dental-services.component.html',
+  styleUrls: ['./dental-services.component.css']
+})
+export class DentalServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
